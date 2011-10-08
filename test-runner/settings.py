1054ae3cb41f76f63bbba3323d578c4ac3d74095
Nic Pottier <nicpottier@gmail.com>
2011-10-08 17:51:39 +0200

    update db settings

--- test-runner/settings.py
+++ test-runner/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tnsglass',                      # Or path to database file if using sqlite3.
-        'USER': 'tnsglass',                      # Not used with sqlite3.
-        'PASSWORD': 'nyaruka',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'smartmin.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
