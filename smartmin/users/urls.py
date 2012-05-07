12416b9a64bb87bb645ef9e9abab4ac8dba30b24
Nic Pottier <nicpottier@gmail.com>
2012-05-07 10:57:39 +0200

    add a LOGOUT_REDIRECT_URL settings to settings.py for those that have that defined

--- smartmin/users/urls.py
+++ smartmin/users/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls.defaults import *
 
 from .views import *
 from django.contrib.auth.views import login, logout
+from django.conf import settings
+
+logout_url = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
 
 urlpatterns = patterns('',
     url(r'^login/$', login, dict(template_name='smartmin/users/login.html'), name="users.user_login"),
-    url(r'^logout/$', logout, dict(redirect_field_name='go'), name="users.user_logout"),
+    url(r'^logout/$', logout, dict(redirect_field_name='go', next_page=logout_url), name="users.user_logout"),
 )
 
 urlpatterns += UserCRUDL().as_urlpatterns()
