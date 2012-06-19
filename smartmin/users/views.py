209a1be59765297f5b9223c4ac58f8295927ec01
Eugene Rwagasore <eugene@nyaruka.com>
2012-06-19 18:37:42 +0200

    Added the user profile view

--- smartmin/users/views.py
+++ smartmin/users/views.py
@@ -34,10 +34,30 @@ class UserForm(forms.ModelForm):
 class UserUpdateForm(UserForm):
     new_password = forms.CharField(label="New Password", widget=forms.PasswordInput, required=False)
 
+class UserProfileForm(UserForm):
+    old_password = forms.CharField(label="Old Password", widget=forms.PasswordInput, required=False)
+    new_password = forms.CharField(label="New Password", widget=forms.PasswordInput, required=False)
+    confirm_new_password = forms.CharField(label="Confirm new Password", widget=forms.PasswordInput, required=False)
+
+    def clean_old_password(self):
+        from django.contrib.auth import authenticate
+        user_cache = authenticate(username=self.instance.username, password=self.data['old_password'])
+        if(len(self.data['old_password']) > 0 and len(self.data['new_password']) > 0):
+            if(user_cache is None):
+                raise forms.ValidationError("The old password is not correct.")
+        elif(len(self.data['old_password']) > 0 and len(self.data['new_password']) == 0):
+            raise forms.ValidationError("Fill new password for changes to take effect")
+        return self.data['old_password']
+
+    def clean_new_password(self):
+        if(self.data['new_password'] != self.data['confirm_new_password']):
+            raise forms.ValidationError("New password doesn't match with its confirmation")
+        return self.data['new_password']
+
 class UserCRUDL(SmartCRUDL):
     model = User
     permissions = True
-    actions = ('create', 'list', 'update')
+    actions = ('create', 'list', 'update', 'profile')
 
     class List(SmartListView):
         search_fields = ('username__icontains','first_name__icontains', 'last_name__icontains')
@@ -100,4 +120,49 @@ class UserCRUDL(SmartCRUDL):
 
             return obj
 
+    class Profile(SmartUpdateView):
+        form_class = UserProfileForm
+        success_message = "User profile saved successfully."
+        fields = ('username', 'old_password', 'new_password', 'confirm_new_password',
+                  'first_name', 'last_name', 'email', 'groups', 'is_active')
+        field_config = {
+            'is_active': dict(help="Whether this user is allowed to log into the site."),
+            'groups': dict(help="Users will only get those permissions that are allowed for their group."),
+            'old_password': dict(help="To reset your password first enter the old password here."),
+            'new_password': dict(help="You can reset your password by entering a new password here."),
+            'confirm_new_password': dict(help="Confirm your new password by entering exactly the new password here."),
+        }
 
+        def has_permission(self, request, *args, **kwargs):
+            has_perm = super(UserCRUDL.Profile, self).has_permission(request, *args, **kwargs)
+            user = request.user
+
+            if (not has_perm):
+                if(user.pk == int(self.kwargs['pk']) and user.is_authenticated()):
+                    return True
+            return has_perm
+
+        def derive_title(self):
+            return "Edit your profile"
+
+        def derive_field_config(self):
+            field_config = super(UserCRUDL.Profile, self).derive_field_config()
+            field_config['username'] = dict(readonly=True, help="You cannot edit your username, this right is reserved for the adminstrator")
+            return field_config
+
+        def derive_fields(self):
+            fields = super(UserCRUDL.Profile, self).derive_fields()
+            fields.remove('groups')
+            fields.remove('is_active')
+            return fields
+
+        def post_save(self, obj):
+            """
+            Make sure our groups are up to date
+            """
+            if 'groups' in self.form.cleaned_data:
+                obj.groups.clear()
+                for group in self.form.cleaned_data['groups']:
+                    obj.groups.add(group)
+
+            return obj
