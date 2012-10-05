3907097eb654cef464038193a1224019fd89eb1f
Nic Pottier <nicpottier@gmail.com>
2012-10-05 12:33:06 +0200

    tweak to pdf rendering to allow rendering of images

--- smartmin/pdf.py
+++ smartmin/pdf.py
@@ -1,5 +1,7 @@
 import StringIO
 from django.http import HttpResponse
+from django.conf import settings
+import os
 
 # Mixin that will change a class based view to render as PDF
 #
@@ -21,7 +23,12 @@ class PDFMixin(object):
         result = StringIO.StringIO()
             
         # now render with pisa as PDF
-        pdf = pisa.pisaDocument(StringIO.StringIO(response.rendered_content.encode("ISO-8859-1")), result)
+        pdf = pisa.pisaDocument(StringIO.StringIO(response.rendered_content.encode("ISO-8859-1")), result, link_callback=fetch_resource)
         if not pdf.err:
             return HttpResponse(result.getvalue(), mimetype='application/pdf')
         return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+
+def fetch_resource(uri, rel):
+    path = os.path.join(settings.STATICFILES_DIRS[0], uri.replace(settings.STATIC_URL, ""))
+    return path
+
