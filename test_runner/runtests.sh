4c6b0cfd56a5a108a66baf118c82d5f9b69c9318
Norbert Kwizera <norkans7@gmail.com>
2013-11-15 10:44:36 +0200

    fix bash script

--- test_runner/runtests.sh
+++ test_runner/runtests.sh
@@ -6,7 +6,6 @@ pip install django-nose
 pip install coverage
 pip install django-celery
 
-cd test-runner
 python manage.py test blog -s --pdb --noinput --with-coverage --cover-package=smartmin --cover-html-dir=../coverage-report --cover-html
 
 
