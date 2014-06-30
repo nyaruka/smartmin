20b0af6419a50ff42fbcd33d3b7395ba7d03f4f9
Eric Newcomer <ericnewcomer@gmail.com>
2014-06-30 10:01:04 +0200

    install sqlite when running tests

--- test-runner/runtests.sh
+++ test-runner/runtests.sh
@@ -5,6 +5,7 @@ python setup.py develop
 pip install django-nose
 pip install coverage
 pip install django-celery
+pip install pysqlite
 
 cd test-runner
 python manage.py test blog -s --pdb --noinput --with-coverage --cover-package=smartmin --cover-html-dir=../coverage-report --cover-html
