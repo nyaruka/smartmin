d3a3ce4382ff6f015d8d5a55591d1cc344af83b5
Nic Pottier <nicpottier@gmail.com>
2016-02-17 16:08:17 -0800

    Use six for unicode/text consistency across python 2.7 and 3.4

--- smartmin/models.py
+++ smartmin/models.py
@@ -7,6 +7,7 @@ import json
 import pytz
 import six
 
+from six import text_type
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -218,12 +219,12 @@ class SmartModel(models.Model):
                         num_errors += 1
 
                 except SmartImportRowError as e:
-                    error_messages.append(dict(line=line_number+1, error=unicode(e)))
+                    error_messages.append(dict(line=line_number+1, error=text_type(e)))
 
                 except Exception as e:
                     if log:
                         traceback.print_exc(100, log)
-                    raise Exception("Line %d: %s\n\n%s" % (line_number, unicode(e), field_values))
+                    raise Exception("Line %d: %s\n\n%s" % (line_number, text_type(e), field_values))
                 line_number += 1
             # only care about the first sheet
             break
