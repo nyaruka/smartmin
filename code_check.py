7ca363185dde1e268da39df94a17db6802e6101c
Norbert Kwizera <norkans7@gmail.com>
2022-12-09 11:54:11 +0200

    Add code check

---
+++ code_check.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+import colorama
+
+parser = argparse.ArgumentParser(description="Code checks")
+parser.add_argument("--debug", action="store_true")
+args = parser.parse_args()
+
+DEBUG = args.debug
+
+
+def cmd(line):
+    if DEBUG:
+        print(colorama.Style.DIM + "% " + line + colorama.Style.RESET_ALL)
+    try:
+        output = subprocess.check_output(line, shell=True).decode("utf-8")
+        if DEBUG:
+            print(colorama.Style.DIM + output + colorama.Style.RESET_ALL)
+        return output
+    except subprocess.CalledProcessError as e:
+        print(colorama.Fore.RED + e.stdout.decode("utf-8") + colorama.Style.RESET_ALL)
+        exit(1)
+
+
+def status(line):
+    print(colorama.Fore.GREEN + f">>> {line}..." + colorama.Style.RESET_ALL)
+
+
+if __name__ == "__main__":
+    colorama.init()
+
+    status("Make any missing migrations")
+    cmd("python manage.py makemigrations")
+
+    status("Running black")
+    cmd("black smartmin test_runner")
+
+    status("Running ruff")
+    cmd("ruff smartmin")
+
+    status("Running isort")
+    cmd("isort smartmin")
+
+    # if any code changes were made, exit with error
+    if cmd("git diff smartmin test_runner"):
+        print("👎 " + colorama.Fore.RED + "Changes to be committed")
+        exit(1)
+    else:
+        print("👍 " + colorama.Fore.GREEN + "Code looks good. Make that PR!")
